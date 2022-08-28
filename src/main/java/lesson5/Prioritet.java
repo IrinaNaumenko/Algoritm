5b47acb042629b5099c0c76b922323bce83ee3ac
IrinaNaumenko <irina1280808@gmail.com>
2022-08-28 14:59:14 +0200

    Removing unnecessary classes

--- src/main/java/lesson5/Prioritet.java
+++
@@ -1,7 +0,0 @@
-package lesson5;
-
-public class Prioritet {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/lesson5/Proba.java
+++
@@ -1,9 +0,0 @@
-package lesson5;
-
-public class Proba {
-    public static void main(String[] args) {
-        System.out.println("proba");
-
-
-    }
-}
