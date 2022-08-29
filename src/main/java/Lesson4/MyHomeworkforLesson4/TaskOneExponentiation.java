07bbc17727fa0f4af3cf2c2f77ea0da182d1dfbf
IrinaNaumenko <irina1280808@gmail.com>
2022-08-29 12:45:30 +0200

    Task 1. Exponentiation of a number.

---
+++ src/main/java/Lesson4/MyHomeworkforLesson4/TaskOneExponentiation.java
@@ -0,0 +1,19 @@
+package Lesson4.MyHomeworkforLesson4;
+
+public class TaskOneExponentiation {
+    public static void main(String[] args) {
+        System.out.println(pov(2,4));
+
+    }
+
+    private static long pov(long x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return  x;
+        }
+        return x * pov(x, n-1);
+    }
+}
+
